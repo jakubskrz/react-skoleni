d225eaa27a1c7eea4fcea8ba6588d0ffb697e5c2
Jakub Skrzeczek <jakub@skrzeczek.cz>
2018-02-12 16:56:26 +0100

    First commit

---
+++ pages/about.js
@@ -0,0 +1,7 @@
+// @flow
+import * as React from 'react';
+import Page from '../components/Page';
+
+const About = () => <Page title="neco">about</Page>;
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+// @flow
+import * as React from 'react';
+import Page from '../components/Page';
+import Text from '../components/Text';
+
+const Index = () => <Page title="welcome">Welcome!</Page>;
+
+export default Index;
