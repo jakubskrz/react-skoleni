511fc6692ef209d274e27d5d9a71b50cea6632ab
Jakub Skrzeczek <jakub@skrzeczek.cz>
2018-02-13 13:37:19 +0100

    Day 2: Auth form component

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,13 @@
 import * as React from 'react';
 import Page from '../components/Page';
 import Text from '../components/Text';
+import Auth from '../components/Auth';
 
-const Index = () => <Page title="welcome">Welcome!</Page>;
+const Index = () => (
+  <Page title="Home">
+    <Text>Welcome to React!</Text>
+    <Auth />
+  </Page>
+);
 
 export default Index;
